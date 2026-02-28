3f04a93cb6dfd8edbfb0f3f4d560e4853f297e90
Yahya Lebar <yahya.lebbar13@gmail.com>
2026-02-28 14:12:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9506506653-9b525",
+  "appId": "1:949158596969:web:76008c3d4edf496806edf8",
+  "apiKey": "AIzaSyD6IfP8tC8KMsYg70yFFbVqwHrdgDtiqTg",
+  "authDomain": "studio-9506506653-9b525.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "949158596969"
+};
